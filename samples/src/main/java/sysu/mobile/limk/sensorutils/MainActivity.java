3baa0a4c67c063db06c3a0e54613027457b9cb7b
Mingkuan Li <limkuan24@gmail.com>
2017-05-14 17:36:12 +0800

    update initial version

---
+++ samples/src/main/java/sysu/mobile/limk/sensorutils/MainActivity.java
@@ -0,0 +1,80 @@
+package sysu.mobile.limk.sensorutils;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+import sysu.mobile.limk.library.CalibrationActivity;
+import sysu.mobile.limk.library.SensorUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button mCalibrateButton;
+    Button mResetButton;
+    TextView mGyroTextView;
+    TextView mCompassTextView;
+
+    SensorUtils mSensorUtils;
+
+    Handler mHandler;
+
+    Runnable mUpdateSensorRunnable = new Runnable() {
+        @Override
+        public void run() {
+            mGyroTextView.setText(String.format(Locale.ENGLISH, "%f", mSensorUtils.getAngle()));
+            mCompassTextView.setText(String.format(Locale.ENGLISH, "%f", mSensorUtils.getCompassDirection()));
+            mHandler.postDelayed(this, 500);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mHandler = new Handler();
+
+        mCalibrateButton = (Button) findViewById(R.id.btn_calibration);
+        mResetButton = (Button) findViewById(R.id.btn_reset);
+        mGyroTextView = (TextView) findViewById(R.id.tv_gyroscope_value);
+        mCompassTextView = (TextView) findViewById(R.id.tv_compasss_value);
+
+        mSensorUtils = SensorUtils.getInstance(getApplicationContext());
+
+        mCalibrateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CalibrationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mResetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSensorUtils.reset();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorUtils.registerSensor();
+        mSensorUtils.reset();
+        mHandler.post(mUpdateSensorRunnable);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorUtils.unregisterSensor();
+        mHandler.removeCallbacks(mUpdateSensorRunnable);
+    }
+}
